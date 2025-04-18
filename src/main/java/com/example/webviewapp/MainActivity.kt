c420e557071afb36326a2c3514307fc6ab8c861f
Abayomi Daniel <56458838+DOVEPCDIABLE@users.noreply.github.com>
2025-04-18 23:06:15 +0100

    Add src/main/java/com/example/webviewapp/MainActivity.kt 1745013975418

---
+++ src/main/java/com/example/webviewapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.webviewapp
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        webView.loadUrl("https://itrad.pybe.com.ng/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
